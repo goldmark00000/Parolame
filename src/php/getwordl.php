e04b1bbb70a2eae51659640c7a80ee6b0aaf1b00
goldmark00000 <goldmark00000@gmail.com>
2022-10-12 21:47:35 +0200

    -#3

--- src/php/getwordl.php
+++ src/php/getwordl.php
@@ -1,11 +1,11 @@
 <?php
 
-$settings = json_decode(file_get_contents('settings.json'),true); 
+$settings = json_decode(file_get_contents('./src/settings.json'),true); 
 
 $host=$settings["host"];
 $user=$settings["user"];
 $password=$settings["password"];
 
 $connection=new mysqli($host, $user, $password);
-echo "ciao";
+
 ?>
