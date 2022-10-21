2b49a43d9e21b11da60063b5dcd143437f7633aa
goldmark00000 <goldmark00000@gmail.com>
2022-10-21 19:37:21 +0200

    .

--- esempi/esempio2.js
+++
@@ -1,9 +0,0 @@
-const stringa = "abcdefghi";
-
-
-console.log(stringa[4]);
-
-// for(let i=0; i< stringa.length; i++){
-//     console.log(stringa[i]);
-// }
-
--- esempi/esempio3.js
+++
@@ -1,9 +0,0 @@
-const selector = document.querySelectorAll("p");
-
-const arrayLength = selector.length;
-
-for (let i = 0; i < arrayLength; i++) {
-    selector[i].textContent = "ciao" + i;
-}
-
-
