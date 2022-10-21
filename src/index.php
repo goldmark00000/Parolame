a7b6cf52f6a699abd2b8394ccc1d9d96e18c10a5
goldmark00000 <goldmark00000@gmail.com>
2022-10-21 19:31:07 +0200

    -#3

--- src/index.php
+++ src/index.php
@@ -1,24 +1,34 @@
 <?php
 
-include "./php/getwordl.php";
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('parolame.db');
+    }
+}
+$db = new MyDB();
+if (!$db) {
+    echo $db->lastErrorMsg();
+} else {
+    echo "Opened database successfully\n";
+}
 
-echo $host;
-echo $user;
-echo $password;
-
-$connection=sqlite_open();
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parolame</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
--- src/php/getwordl.php
+++ src/php/getwordl.php
@@ -1,10 +1,6 @@
 <?php
 
-$settings = json_decode(file_get_contents('./src/settings.json'),true); 
 
-$host=$settings["host"];
-$user=$settings["user"];
-$password=$settings["password"];
 
 
 
