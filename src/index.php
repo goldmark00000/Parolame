730984f0618880449deac02c31674f41774f8ea0
goldmark00000 <goldmark00000@gmail.com>
2022-10-27 17:26:30 +0200

    .

--- src/index.php
+++ src/index.php
@@ -1,16 +1,6 @@
 <?php
 
-class MyDB extends SQLite3
-{
-    function __construct()
-    {
-        $this->open('parolame.db');
-    }
-}
-$db = new MyDB();
-if (!$db) {
-    echo $db->lastErrorMsg();
-}
+
 
 
 
