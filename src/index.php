30ac437ae0c39ffa6e85bb8673b2af5b1badfd36
goldmark00000 <goldmark00000@gmail.com>
2022-11-03 19:18:56 +0100

    #3

--- src/index.php
+++ src/index.php
@@ -1,6 +1,12 @@
 <?php
-if (!file_exists("./php/Subject.php")) exit();
-if (!file_exists("./php/create_account.php")) exit();
+$pathFiles=array("./php/Subject.php","./php/create_account.php");
+$pathFilesLength=count($pathFiles);
+for($i=0; $i<$pathFilesLength; $i++){
+    if (!file_exists($pathFiles[$i])){
+        echo "File not found! ". $pathFiles[$i];
+        exit();
+    }   
+}
 
 if (!isset($_COOKIE["ID_user"])) {
     echo "sei nuovo";
@@ -20,10 +26,7 @@ try {
     echo $e;
     exit();
 }
-echo $subject;
-
 ?>
-
 <!DOCTYPE html>
 <html lang="it">
 
@@ -32,8 +35,8 @@ echo $subject;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parolame</title>
-    <script src="index.js"></script>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./styles/index.css">
+    <script src="./scripts/index.js" defer></script>
 </head>
 
 <body>
---
+++ src/login.php
@@ -0,0 +1,30 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/index.css">
+    <script src="./scripts/index.js" defer></script>
+    <title>Parolame</title>
+</head>
+<body>
+    <div class="container">
+        <div class="elements-container">
+            <div class="text-container">
+                <label class="" for="idUser">Nome utente:</label>
+                <input type="text" class="id-user" id="idUser"></input>
+                <label class="" for="passwordUser">Password:</label>
+                <input type="password" class="password-user" id="passwordUser"></input>
+            </div>
+            <button class="confirm-button"></button>
+        </div>
+        <p class="link-create-acc">Vuoi creare un nuovo account?</p>
+    </div>
+</body>
+</html>
