b92d1a979c6eea0ba4fc7f8ecb37979630a6f58c
goldmark00000 <goldmark00000@gmail.com>
2022-11-15 21:52:11 +0100

    .

--- src/php/create_account.php
+++ src/php/create_account.php
@@ -1,15 +1,10 @@
 <?php
     echo "sono create account";
-?>
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parolame</title>
-</head>
-<body>
+
+
+
+
+
+
     
-</body>
-</html>
+?>
