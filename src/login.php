ca307d4309e0c3a5f76a8c4e1424979722e27a2d
goldmark00000 <goldmark00000@gmail.com>
2022-11-17 20:03:39 +0100

    #1

--- src/login.php
+++ src/login.php
@@ -1,7 +1,6 @@
 <?php
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,27 +8,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/palette.css">
     <link rel="stylesheet" href="./styles/login.css">
     <script src="./scripts/login.js" defer></script>
     <title>Parolame</title>
 </head>
 
 <body>
-    <div class="login-container">
-        <div class="elements-container">
-            <div class="text-container">
-                <span class="text-input">
-                    <label class="" for="idUser">Nome utente:</label>
-                    <input type="text" class="id-user" id="idUser"></input>
-                </span>
-                <span class="text-input">
-                    <label class="" for="passwordUser">Password:</label>
-                    <input type="password" class="password-user" id="passwordUser"></input>
-                </span>
+    <div class="background-container">
+        <div class="login-container">
+            <div class="elements-container">
+                <div class="text-container">
+                    <span class="text-input">
+                        <label for="idUser">Nome utente:</label>
+                        <input type="text" class="id-user" id="idUser"></input>
+                    </span>
+                    <span class="text-input">
+                        <label for="passwordUser">Password:</label>
+                        <input type="password" class="password-user" id="passwordUser"></input>
+                    </span>
+                </div>
+                <button class="confirm-button">Confirm</button>
             </div>
-            <button class="confirm-button">Confirm</button>
+            <p class="link-create-acc">Vuoi creare un nuovo account?</p>
         </div>
-        <p class="link-create-acc">Vuoi creare un nuovo account?</p>
     </div>
 </body>
 
